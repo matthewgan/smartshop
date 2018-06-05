f030feed2494f1b94a979f53d6191415bc250463
Matthew Gan <matthewgan@126.com>
2018-06-05 21:33:10 +0800

    fix wechat payment bug using service supplier

--- payments/models.py
+++ payments/models.py
@@ -13,6 +13,31 @@ from bs4 import BeautifulSoup
 # Imports from your apps
 
 
+# Global setting given by wechat
+def get_wechat_app_id():
+    return 'wxff1e7b77ac356972'
+
+
+def get_wechat_sub_app_id():
+    return 'wx18902f96ec8fb847'
+
+
+def get_wechat_mch_id():
+    return '1484700102'
+
+
+def get_wechat_sub_mch_id():
+    return '1505139251'
+
+
+def get_service_api_key():
+    return 'PRiiXyGL0ULPRiiXyGL0UL8888888888'
+
+
+def get_notify_url():
+    return 'https://www.wuzhanggui.shop/api/payment/notify/'
+
+
 def isoformat(time):
     '''
     将datetime或者timedelta对象转换成ISO 8601时间标准格式字符串
@@ -72,65 +97,121 @@ def trans_dict_to_xml(data):
 
 
 def PayOrderByWechat(fee, out_trade_no, openid):
-    appid = 'wx18902f96ec8fb847'
+
+    # static data need to be put in setting
+    appid = get_wechat_app_id()
+    sub_appid = get_wechat_sub_app_id()
+    sub_mchid = get_wechat_sub_mch_id()
+    mch_id = get_wechat_mch_id()
+    api_key = get_service_api_key()
+    notify_url = get_notify_url()
+    tencent_unifiedorder_api = 'https://api.mch.weixin.qq.com/pay/unifiedorder'
+
+    # prepare the data for the Tencent API
     body = '物掌柜智慧便利'
-    mch_id = '1505139251'
     nonce_str = str(random.random()*10)
-    notify_url = 'https://www.wuzhanggui.shop/api/payment/notify/'
     ip = str(get_host_ip())
     fee = str(int(fee*100))
-    stringA = "appid=" + appid + "&body=" + body + "&mch_id=" + mch_id + "&nonce_str=" + nonce_str + "&notify_url=" + notify_url + "&openid=" + openid + "&out_trade_no=" + out_trade_no + "&spbill_create_ip=" + ip + "&total_fee=" + fee + "&trade_type=JSAPI"
-    stringSignTemp = stringA + "&key=0C99EDFF5CC4C3B4580EF329368F14DA"
-    paysign = hashlib.md5(stringSignTemp.encode('utf-8')).hexdigest().upper()
-    pay_xml = "<xml><appid>" + appid + "</appid><body>" + body + "</body><mch_id>" + mch_id + "</mch_id><nonce_str>" + nonce_str + "</nonce_str><notify_url>" + notify_url + "</notify_url><openid>" + openid + "</openid><out_trade_no>" + out_trade_no + "</out_trade_no><spbill_create_ip>" + ip + "</spbill_create_ip><total_fee>" + fee + "</total_fee><trade_type>JSAPI</trade_type><sign>" + paysign + "</sign></xml> "
 
-    req = urllib.request.Request("https://api.mch.weixin.qq.com/pay/unifiedorder")
-    req.add_header('User-Agent',
-                   'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25')
-    ssl._create_default_https_context = ssl._create_unverified_context
-    try:
-        unifiedorderXML = urllib.request.urlopen(req, data=pay_xml.encode('utf-8'), timeout=3)
-    except:
-        print('Tencent Server Error')
+    # sign for the data user MD5
+    stringA = "appid=" + appid + "&body=" + body + "&mch_id=" + mch_id + "&nonce_str=" + nonce_str + "&notify_url=" \
+            + notify_url  + "&out_trade_no=" + out_trade_no + "&spbill_create_ip=" + ip + "&sub_appid=" + sub_appid \
+            + "&sub_mch_id=" + sub_mchid + "&sub_openid=" + openid + "&total_fee=" + fee + "&trade_type=JSAPI"
+    stringSignTemp = stringA + "&key=" + api_key
+    paysign = hashlib.md5(stringSignTemp.encode('utf-8')).hexdigest().upper()
+
+    # generate the xml
+    orderquery = {
+        'appid': appid,
+        'mch_id': mch_id,
+        'sub_mch_id': sub_mchid,
+        'sub_appid': sub_appid,
+        'sub_openid': openid,
+        'body': body,
+        'nonce_str': nonce_str,
+        'notify_url': notify_url,
+        'out_trade_no': out_trade_no,
+        'spbill_create_ip': ip,
+        'total_fee': fee,
+        'trade_type':'JSAPI',
+        'sign': paysign,
+    }
+    xml = trans_dict_to_xml(orderquery)
+
+    error = 0
+    while error < 3:
+        try:
+            resp = requests.post(tencent_unifiedorder_api, data=xml.encode('utf-8'), headers={'Content-Type': 'text/xml'})
+            break
+        except:
+            error += 1
+    if error == 3:
         return 400
 
-    res = trans_xml_to_dict(unifiedorderXML)
-    timestamp = str(time.time())
-    timestamp = timestamp[0:10]
-    psign = res.get('sign')
-    prepay_id = res.get('prepay_id')
-    package = "prepay_id=" + prepay_id
-    stringB = "appId=wx18902f96ec8fb847&nonceStr="+nonce_str+"&package=" + package + "&signType=MD5&timeStamp=" + timestamp
-    stringBSignTemp = stringB + "&key=0C99EDFF5CC4C3B4580EF329368F14DA"
-    paysign = hashlib.md5(stringBSignTemp.encode('utf-8')).hexdigest().upper()
-    toWxApp = {'timeStamp': timestamp, 'nonceStr': nonce_str, 'package': package, 'signType': 'MD5', 'paySign': paysign,
-               'tradeNo': out_trade_no, 'status': 1}
+    # get the data for wx.payment
+    msg = resp.text.encode('ISO-8859-1').decode('utf-8')
+    res = trans_xml_to_dict(msg)
+    if res.get('return_code') == 'SUCCESS':
+        timestamp = str(time.time())
+        timestamp = timestamp[0:10]
+        psign = res.get('sign')
+        prepay_id = res.get('prepay_id')
+        package = "prepay_id=" + prepay_id
+        stringB = "appId=" + sub_appid + "&nonceStr=" \
+                  + nonce_str + "&package=" + package \
+                  + "&signType=MD5&timeStamp=" + timestamp
+        stringBSignTemp = stringB + "&key=" + api_key
+        paysign = hashlib.md5(stringBSignTemp.encode('utf-8')).hexdigest().upper()
+        toWxApp = {
+            'timeStamp': timestamp,
+            'nonceStr': nonce_str,
+            'package': package,
+            'signType': 'MD5',
+            'paySign': paysign,
+            'tradeNo': out_trade_no,
+            'status': 1
+        }
+    else:
+        toWxApp = {'return_msg': res.get('return_msg')}
+
     return toWxApp
 
 
 def OrderQuery(out_trade_no):
-    appid = 'wx18902f96ec8fb847'
-    mch_id = '1505139251'
+
+    # static data need to be put in setting
+    appid = get_wechat_app_id()
+    sub_appid = get_wechat_sub_app_id()
+    sub_mchid = get_wechat_sub_mch_id()
+    mch_id = get_wechat_mch_id()
+    tencent_orderquery_api = 'https://api.mch.weixin.qq.com/pay/orderquery'
+
+    # prepare the data for the Tencent API
     nonce_str = str(random.random()*10)
-    stringA = "appid=" + appid +"&mch_id=" + mch_id + "&nonce_str=" + nonce_str + "&out_trade_no=" + out_trade_no
-    stringSignTemp = stringA + "&key=0C99EDFF5CC4C3B4580EF329368F14DA"
+
+    # sign for the data user MD5
+    stringA = "appid=" + appid +"&mch_id=" + mch_id + "&nonce_str=" + nonce_str + "&out_trade_no=" \
+              + out_trade_no + '&sub_appid=' +sub_appid + "&sub_mch_id=" + sub_mchid
+    stringSignTemp = stringA + "&key=" + get_service_api_key()
     paysign = hashlib.md5(stringSignTemp.encode('utf-8')).hexdigest().upper()
 
+    # generate the xml
     orderquery = {
         'appid': appid,
         'mch_id': mch_id,
+        'sub_mch_id': sub_mchid,
+        'sub_appid': sub_appid,
         'nonce_str': nonce_str,
         'out_trade_no': out_trade_no,
         'sign': paysign,
-
     }
     xml = trans_dict_to_xml(orderquery)
-    print(xml)
+
+    # request for tencent orderquery api
     error = 0
-    while error<3:
+    while error < 3:
         try:
-            resp = requests.post("https://api.mch.weixin.qq.com/pay/orderquery", data=xml.encode('utf-8'),
-                                  headers={'Content-Type': 'text/xml'})
+            resp = requests.post(tencent_orderquery_api, data=xml.encode('utf-8'), headers={'Content-Type': 'text/xml'})
             break
         except:
             error += 1
@@ -138,6 +219,7 @@ def OrderQuery(out_trade_no):
         resdata = {'status': 400}
         return resdata
 
+    # get the response data
     msg = resp.text.encode('ISO-8859-1').decode('utf-8')
     xmlresp = trans_xml_to_dict(msg)
 
@@ -162,6 +244,5 @@ def OrderQuery(out_trade_no):
             'return_code': xmlresp.get('return_code'),
             'return_msg': xmlresp.get('return_msg'),
         }
-    #print(resdata)
-    return resdata
 
+    return resdata
