87f7010a25a87dac5fb056d1f06647d96009dfa0
roxasw <roxas.wang@live.com>
2018-07-24 14:23:45 +0800

    -- fix media .JPG

--- merchandises/serializers.py
+++ merchandises/serializers.py
@@ -10,7 +10,7 @@ class MerchandiseListShowInfoSerializer(serializers.ModelSerializer):
     picture = serializers.SerializerMethodField('render_picture_url')
 
     def render_picture_url(self, obj):
-        fullurl = 'https://www.wuzhanggui.shop/media/' + obj.code + '.jpg'
+        fullurl = 'https://www.wuzhanggui.shop/media/' + obj.code + '.JPG'
         return fullurl
 
     class Meta:
